b4d8db3775854be737323f2d9853bb1b9e41963d
kuniaki <k.ishikawa.0128@gmail.com>
2020-08-18 11:16:20 +0900

    create authro profile on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import logo from '../img/social/facebook.svg'
 
 const Sidebar = class extends React.Component {
   render() {
     return (
-      <div className="columns is-4">
-        hoge
+      <div className="columns is-multiline">
+        <div className="column is-12 has-text-centered">
+          <article className="is-child box">
+            <img
+              src={logo}
+              alt="Kaldi"
+              style={{ width: '6em', height: '6em', borderRadius: '4em' }}
+            />
+            <h3>Author Name</h3>
+            <p>
+              hogehogehogehogehoge
+              hogehogehogehogehoge
+              hogehogehogehogehoge
+              hogehogehogehogehoge
+            </p>
+          </article>
+        </div>
       </div>
     )
   }
