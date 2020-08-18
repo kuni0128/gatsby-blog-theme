cb5f3066ec1d13d48d8688247874f3ed99561589
kuniaki <k.ishikawa.0128@gmail.com>
2020-08-18 16:44:51 +0900

    refactor to commonize Posts component

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import { graphql, StaticQuery } from 'gatsby'
+import Posts from './Posts'
 
 class BlogRoll extends React.Component {
   render() {
@@ -9,48 +9,7 @@ class BlogRoll extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-12" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  <p className="post-meta">
-                    <span className="subtitle is-size-12 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                  </p>
-                  <div className="featured-thumbnail">
-                    <PreviewCompatibleImage
-                      imageInfo={{
-                        image: post.frontmatter.featuredimage,
-                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                      }}
-                    />
-                  </div>
-                </header>
-                <p className="has-text-centered">
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    READ MORE
-                  </Link>
-                </p>
-              </article>
-            </div>
-          ))}
-      </div>
+      <Posts posts={posts} />
     )
   }
 }
--- src/templates/period-summary-page.js
+++ src/templates/period-summary-page.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Sidebar from '../components/Sidebar'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import Posts from '../components/Posts'
 
 const PeriodSummaryPage = ({
   data: {
-    allMarkdownRemark: { totalCount, edges: posts },
-    site: {
-      siteMetadata: { title },
-    },
+    allMarkdownRemark: { edges: posts },
   },
 }) => (
   <Layout>
@@ -20,50 +17,7 @@ const PeriodSummaryPage = ({
             <div className="content">
               <div className="columns is-multiline">
                 <div className="column is-8">
-
-                  <div className="columns is-multiline">
-                    {posts &&
-                      posts.map(({ node: post }) => (
-                        <div className="is-parent column is-12" key={post.id}>
-                          <article
-                            className={`blog-list-item tile is-child box notification ${
-                              post.frontmatter.featuredpost ? 'is-featured' : ''
-                            }`}
-                          >
-                            <header>
-                              <p className="post-meta">
-                                <span className="subtitle is-size-12 is-block">
-                                  {post.frontmatter.date}
-                                </span>
-                                <Link
-                                  className="title has-text-primary is-size-4"
-                                  to={post.fields.slug}
-                                >
-                                  {post.frontmatter.title}
-                                </Link>
-                              </p>
-                              <div className="featured-thumbnail">
-                                <PreviewCompatibleImage
-                                  imageInfo={{
-                                    image: post.frontmatter.featuredimage,
-                                    alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                  }}
-                                />
-                              </div>
-                            </header>
-                            <p className="has-text-centered">
-                              {post.excerpt}
-                              <br />
-                              <br />
-                              <Link className="button" to={post.fields.slug}>
-                                READ MORE
-                              </Link>
-                            </p>
-                          </article>
-                        </div>
-                      ))}
-                  </div>
-
+                  <Posts posts={posts} />
                 </div>
                 <div className="column is-4">
                   <Sidebar />
@@ -79,44 +33,8 @@ const PeriodSummaryPage = ({
 
 export default PeriodSummaryPage
 
-// PeriodSummaryPageTemplate.propTypes = {
-//   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-//   title: PropTypes.string,
-//   subheading: PropTypes.string,
-//   intro: PropTypes.shape({
-//     blurbs: PropTypes.array,
-//   }),
-// }
-
-// const PeriodSummaryPage = ({ data }) => {
-//   const { allMarkdownRemark, site } = data
-
-//   return (
-//     <Layout>
-//       <PeriodSummaryPageTemplate
-//         title={}
-//       />
-//     </Layout>
-//   )
-// }
-
-// PeriodSummaryPage.propTypes = {
-//   data: PropTypes.shape({
-//     markdownRemark: PropTypes.shape({
-//       frontmatter: PropTypes.object,
-//     }),
-//   }),
-// }
-
-// export default PeriodSummaryPage
-
 export const periodSummaryPageQuery = graphql`
   query ($periodStartDate: Date, $periodEndDate: Date) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     allMarkdownRemark(
       limit: 200
       sort: { fields: [frontmatter___date], order: DESC }
@@ -124,7 +42,6 @@ export const periodSummaryPageQuery = graphql`
         frontmatter: { date: { gte: $periodStartDate, lt: $periodEndDate } }
       }
     ) {
-      totalCount
       edges {
         node {
           excerpt(pruneLength: 400)
