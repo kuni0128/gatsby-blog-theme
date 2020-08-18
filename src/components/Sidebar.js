28abb9cc2900ecc0f871e2b7220fe8cf07afc1a0
kuniaki <k.ishikawa.0128@gmail.com>
2020-08-18 10:58:31 +0900

    fix layout for sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = class extends React.Component {
+  render() {
+    return (
+      <div className="columns is-4">
+        hoge
+      </div>
+    )
+  }
+}
+
+export default Sidebar
