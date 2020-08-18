297a4ec7aa036825de888896714af5bd87b4001b
kuniaki <k.ishikawa.0128@gmail.com>
2020-08-18 09:50:29 +0900

    modify to get only 5 blog

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -17,9 +17,6 @@ export const IndexPageTemplate = ({
             <div className="content">
               <Features gridItems={intro.blurbs} />
               <div className="column is-12">
-                <h3 className="has-text-weight-semibold is-size-2">
-                  Latest stories
-                </h3>
                 <BlogRoll />
                 <div className="column is-12 has-text-centered">
                   <Link className="btn" to="/blog">
