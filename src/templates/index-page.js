150fd0428a78965be0cf58b195a431fe1cdec63a
kuniaki <k.ishikawa.0128@gmail.com>
2020-08-18 09:35:19 +0900

    sectionでは無くarticleタグに変更

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -12,21 +12,19 @@ export const IndexPageTemplate = ({
   <div>
     <section className="section section--gradient">
       <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <Features gridItems={intro.blurbs} />
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="content">
+              <Features gridItems={intro.blurbs} />
+              <div className="column is-12">
+                <h3 className="has-text-weight-semibold is-size-2">
+                  Latest stories
+                </h3>
+                <BlogRoll />
+                <div className="column is-12 has-text-centered">
+                  <Link className="btn" to="/blog">
+                    Read more
+                  </Link>
                 </div>
               </div>
             </div>
