dc336f80382916a36d01fcbc3283df4458800d21
kuniaki <k.ishikawa.0128@gmail.com>
2020-08-19 10:22:20 +0900

    introduce sidebar on blog page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,34 +1,28 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
+import Sidebar from '../../components/Sidebar'
 import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-            }}
-          >
-            Latest Stories
-          </h1>
-        </div>
         <section className="section">
           <div className="container">
-            <div className="content">
-              <BlogRoll />
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="content">
+                  <div className="columns is-multiline">
+                    <div className="column is-8">
+                      <BlogRoll />
+                    </div>
+                    <div className="column is-4">
+                      <Sidebar />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { kebabCase } from 'lodash'
 import { Helmet } from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
+import Sidebar from '../components/Sidebar'
 import Content, { HTMLContent } from '../components/Content'
 
 export const BlogPostTemplate = ({
@@ -19,26 +20,35 @@ export const BlogPostTemplate = ({
   return (
     <section className="section">
       {helmet || ''}
-      <div className="container content">
+      <div className="container">
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
+            <div className="content">
+              <div className="columns is-multiline">
+                <div className="column is-8">
+                  <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+                    {title}
+                  </h1>
+                  <p>{description}</p>
+                  <PostContent content={content} />
+                  {tags && tags.length ? (
+                    <div style={{ marginTop: `4rem` }}>
+                      <h4>Tags</h4>
+                      <ul className="taglist">
+                        {tags.map((tag) => (
+                          <li key={tag + `tag`}>
+                            <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ) : null}
+                </div>
+                <div className="column is-4">
+                  <Sidebar />
+                </div>
               </div>
-            ) : null}
+            </div>
           </div>
         </div>
       </div>
